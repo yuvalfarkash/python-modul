b8f0ff7e4fcc4fbae46d0ef0a1f52e00b35e8d12
yuvalfarkash <yuvalfarkash85@gmail.com>
2026-03-29 10:06:49 +0300

    adding for_loops file

---
+++ loops/for_loops.py
@@ -0,0 +1,15 @@
+# while loops basics
+# user_input = input("Would you like to paly? (n/y)")
+
+# user_input = "y"
+# while user_input != "n":
+#     print("Game is Running...")
+    # user_input = input("Would you like to play again (n/y)")
+    
+    
+while True:
+    user_input = input("Would you like to paly? (n/y)")
+    
+    if user_input == "n":
+        break
+    print("Game is runing")
