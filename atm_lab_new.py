6be6c46199f477da9dfeeb1f0645f03113bd5e76
yuvalfarkash <yuvalfarkash85@gmail.com>
2026-03-30 11:58:26 +0300

    [feature] atm app

--- atm_lab_new.py
+++ atm_lab_new.py
@@ -1,46 +1,57 @@
-user_account = {
+accounts = [{
     "account_id": 1001,
     "username": "Yuval",
     "balance": 55000,
     "pin": 3571
-}
+},
+{
+    "account_id": 1002,
+    "username": "Mike",
+    "balance": 100000,
+    "pin": 1234
+}]
 
-def auth(account_pin):
+def auth():
+    id = int(input("Enter your account id"))
     pin = int(input("enter your PIN: "))
-    if account_pin == pin:
-        return True
-    else:
-        return False
-        
+    
+    # Find matching account from id
+    for user in accounts:
+        if id == user["account_id"] and user["pin"] == pin:
+            print(f"Welcome {user["username"]}")
+            return user # exists => positive => True => 1
+        else:
+            return None # non-exisits => negative => False => 0
 
 def withdraw():
-    if auth(user_account["pin"]):
+    user_account = auth()
+    if user_account:
         amount = float(input("Please type the amount you would like to withdraw: "))
         if amount > user_account["balance"]:
             print("You are poor")
         else:
             user_account["balance"] -= amount
-            balance()
+            balance(user_account["balance"])
     else:
         print("Wrong PIN, goodbye")
         exit()
 
 def deposite():
-    if auth(user_account["pin"]):
+    user_account = auth()
+    if user_account:
         amount = float(input("Please type the amount you would like to deposite: "))
         if amount < 0:
             print("You cannot tpye negative numbers")
             exit()
         else:
             user_account["balance"] += amount
-            balance()
+            balance(user_account["balance"])
     else:
         print("Wrong PIN, goodbye")
         exit()
 
-def balance():
-    print(f"Your Balance now is {user_account["balance"]}")
-    pass
+def balance(user_balance):
+    print(f"Your Balance now is {user_balance}")
 
 
 while True:
